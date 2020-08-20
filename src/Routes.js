1f24e43be761a6c73deca7870807dd83889c54af
오승하 <lilyoh628@gmail.com>
2020-08-20 15:02:49 +0900

    Fix: Routes.js 파일 수정 - Footer 작업을 위해 주석 처리했던 부분 원복

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Footer from "./Components/Footer/Footer";
 import Main from "./Pages/Main/Main";
 import Login from "./Pages/Login/Login";
 import ProductList from "./Pages/ProductList/ProductList";
@@ -11,10 +10,10 @@ class Routes extends Component {
     return (
       <Router>
         <Switch>
-          <Route exact path="/" component={Footer} />
-          {/* <Route exact path="/account" component={Login} />
+          <Route exact path="/" component={Main} />
+          <Route exact path="/account" component={Login} />
           <Route exact path="/shop" component={ProductList} />
-          <Route exact path="/detail" component={ProductDetail} /> */}
+          <Route exact path="/detail" component={ProductDetail} />
         </Switch>
       </Router>
     );
