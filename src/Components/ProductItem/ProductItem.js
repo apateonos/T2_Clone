f9f88cf8730ec9e7a430e6324308ea62f1818f37
kangyeji <kyj07030911@gmail.com>
2020-08-20 13:30:01 +0900

    Add : ProductItem, ProductList 레이아웃 - productItem 레이아웃 - productList 파일에서 productItem component 적용하기 완료

--- src/Components/ProductItem/ProductItem.js
+++ src/Components/ProductItem/ProductItem.js
@@ -1,43 +1,71 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import "./ProductItem.scss";
 
 class ProductItem extends Component {
   constructor() {
     super();
-
-    this.state = {
-      product: [
-        {
-          url:
-            "https://www.t2tea.com/dw/image/v2/AASF_PRD/on/demandware.static/-/Sites-masterCatalog_t2/default/dw77631367/images/products/2020 Aug/T125AE281_blueberry-crumble_p2.png?sw=262&sh=262&sm=fit",
-          name: "Blueberry Cruble Loose Leaf Feature Cube",
-          price: 26.0,
-        },
-      ],
-    };
   }
+
+  handleClick = (e) => {
+    console.log(e);
+    console.log("click!");
+  };
+
   render() {
-    const { product } = this.state;
+    const { product } = this.props;
     return (
       <li className="ProductItem">
         <div className="heartBox">
           <Link>
-            <img src="" alt="heart" />
+            <img
+              src="https://www.t2tea.com/on/demandware.static/Sites-UNI-T2-APAC-Site/-/en_AU/v1597816184082/images/heart.svg"
+              alt="heart"
+            />
+            <div></div>
           </Link>
         </div>
         <div className="imageBox">
           <img src={product.url} alt={product.name} />
         </div>
-        <Link className="productName">{product.name}</Link>
-        <>
-          <img alt="starRatings" src="" />
-          "(3)"
-        </>
-        <>
-          <Link className="viewDetail">View details</Link>
-        </>
-        <div className="ProductOption"></div>
-        <div>A${product.price}</div>
+        <div className="productInfo">
+          <Link className="productName">{product.name}</Link>
+          <div className="ratingBox">
+            <img alt="starRatings" src={product.rating} />
+            (3)
+          </div>
+          <>
+            <Link className="viewDetail">View details</Link>
+          </>
+          <ul className="productVariation">
+            {product.Variation.map((el) => {
+              return (
+                <li key={el.name}>
+                  <button
+                    className="variationImgButton"
+                    name={el.name}
+                    onClick={() => {
+                      this.handleClick(el.name); //set.status 하기
+                    }}
+                  >
+                    <img alt="" src={el.url} />
+                  </button>
+                  <div className="variationName">{el.name}</div>
+                  <div className="price">A${el.price}</div>
+                </li>
+              );
+            })}
+          </ul>
+          <div className="addTobagBox">
+            <select>
+              <option value="1">1</option> <option value="2">2</option>
+              <option value="3">3</option> <option value="4">4</option>
+              <option value="5">5</option> <option value="6">6</option>
+              <option value="7">7</option> <option value="8">8</option>
+            </select>
+            <button>Add to Bag</button>
+          </div>
+        </div>
       </li>
     );
   }
--- src/Pages/ProductList/ProductList.js
+++ src/Pages/ProductList/ProductList.js
@@ -1,29 +1,73 @@
 import React, { Component } from "react";
 import ProductItem from "./../../Components/ProductItem/ProductItem";
+import "./ProductList.scss";
 
 class ProductList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      product: [
+        {
+          url:
+            "https://www.t2tea.com/dw/image/v2/AASF_PRD/on/demandware.static/-/Sites-masterCatalog_t2/default/dw77631367/images/products/2020 Aug/T125AE281_blueberry-crumble_p2.png?sw=262&sh=262&sm=fit",
+          name: "Blueberry Cruble Loose Leaf Feature Cube",
+          rating:
+            "https://www.t2tea.com/on/demandware.static/Sites-UNI-T2-APAC-Site/-/default/dwa0a93cd3/images/stars/rating-4_8.gif",
+          Variation: [
+            {
+              name: "",
+              price: "26.0",
+              url:
+                "https://www.t2tea.com/dw/image/v2/AASF_PRD/on/demandware.static/-/Sites-masterCatalog_t2/default/dw77631367/images/products/2020 Aug/T125AE281_blueberry-crumble_p2.png?sw=80&sh=80&sm=fit",
+            },
+          ],
+        },
+        {
+          url:
+            "https://www.t2tea.com/dw/image/v2/AASF_PRD/on/demandware.static/-/Sites-masterCatalog_t2/default/dw5c3ae3cb/images/UXProject/new-tea-images/T110AI012_matcha_r1.jpg?sw=262&sh=262&sm=fit",
+          name: " 100% Certified Organic Matcha",
+          rating:
+            "https://www.t2tea.com/on/demandware.static/Sites-UNI-T2-APAC-Site/-/default/dw3147e413/images/stars/rating-4_6.gif",
+          Variation: [
+            {
+              name: "Tin 30g-1.1oz",
+              price: "26.00",
+              url:
+                "https://www.t2tea.com/dw/image/v2/AASF_PRD/on/demandware.static/-/Sites-masterCatalog_t2/default/dw2cba5a78/images/T110AI012_matcha-black-tin_p1.png?sw=55&sh=55&sm=fit",
+            },
+            {
+              name: "Tin 50g-1.7oz",
+              price: "44.0",
+              url:
+                "https://www.t2tea.com/dw/image/v2/AASF_PRD/on/demandware.static/-/Sites-masterCatalog_t2/default/dwe989f3ae/images/products/T110AI029_matcha_sha1.png?sw=80&sh=80&sm=fit",
+            },
+          ],
+        },
+      ],
+    };
+  }
+
   render() {
     return (
       <div className="ProductList">
-        <header>
-          <ul>
-            <li>HOME</li>
-            <li>TEA</li>
-            <li>SHOP ALL TEA</li>
-          </ul>
-          <div>Shop all Tea</div>
-        </header>
-        <div>
-          Refine by:
-          <slect></slect>
+        <div className="productContainer">
+          <header>
+            <ul>
+              <li>HOME</li>
+              <li>TEA</li>
+              <li>SHOP ALL TEA</li>
+            </ul>
+            <div className="mainHeading">Shop all Tea</div>
+          </header>
+          <div className="refineByContainer">Refine by:</div>
+          <section>
+            <ul>
+              {this.state.product.map((el) => {
+                return <ProductItem product={el} key={el.name} />;
+              })}
+            </ul>
+          </section>
         </div>
-
-        <section>
-          <div> 리스트 출력하기 </div>
-          <ul>
-            <ProductItem />
-          </ul>
-        </section>
       </div>
     );
   }
