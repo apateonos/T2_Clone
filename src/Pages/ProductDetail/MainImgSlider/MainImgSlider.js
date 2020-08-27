b28cac7477d01725ab87e119095bcf32d05e3f21
오승하 <lilyoh628@gmail.com>
2020-08-27 12:23:41 +0900

    Add: react slick 설치

---
+++ src/Pages/ProductDetail/MainImgSlider/MainImgSlider.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import Slider from "react-slick";
+
+function NextArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "red" }}
+      onClick={onClick}
+    />
+  );
+}
+
+function PrevArrow(props) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{ ...style, display: "block", background: "green" }}
+      onClick={onClick}
+    />
+  );
+}
+
+export default class MainImgSlider extends Component {
+  render() {
+    const settings = {
+      // dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      nextArrow: <NextArrow />,
+      prevArrow: <PrevArrow />,
+    };
+
+    return (
+      <div className="MainImgSlider">
+        {/* <h2>Single Item</h2> */}
+        <Slider {...settings}>
+          <div>
+            {this.props.product_detail.map((el) => {
+              return <img alt="product" src={el.product_image}></img>;
+            })}
+          </div>
+          <div>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+        </Slider>
+      </div>
+    );
+  }
+}
