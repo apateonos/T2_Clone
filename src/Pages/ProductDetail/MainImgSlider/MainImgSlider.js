a2713fa7650782929a7519c1f87dedb1a735539a
오승하 <lilyoh628@gmail.com>
2020-08-27 20:42:14 +0900

    Add: 슬라이더 및 tab 구현 - 메인 이미지 슬라이더 구현 (react slick 활용) - 제품 상세 설명 tab 구현

--- src/Pages/ProductDetail/MainImgSlider/MainImgSlider.js
+++ src/Pages/ProductDetail/MainImgSlider/MainImgSlider.js
@@ -6,7 +6,11 @@ function NextArrow(props) {
   return (
     <div
       className={className}
-      style={{ ...style, display: "block", background: "red" }}
+      // style={{ ...style, display: "block", background: "red" }}
+      // style={{
+      //   backgroundImage:
+      //     "https://www.t2tea.com/on/demandware.static/Sites-UNI-T2-APAC-Site/-/en_AU/v1598427800341/images/pdp-right.png",
+      // }}
       onClick={onClick}
     />
   );
@@ -17,7 +21,7 @@ function PrevArrow(props) {
   return (
     <div
       className={className}
-      style={{ ...style, display: "block", background: "green" }}
+      // style={{ ...style, display: "block", background: "green" }}
       onClick={onClick}
     />
   );
@@ -34,34 +38,63 @@ export default class MainImgSlider extends Component {
       nextArrow: <NextArrow />,
       prevArrow: <PrevArrow />,
     };
+    //console.log(this.props.product_detail[0]);
+
+    const { product_detail } = this.props;
 
     return (
       <div className="MainImgSlider">
-        {/* <h2>Single Item</h2> */}
+        {/* {product_detail.product_image_big && ( */}
         <Slider {...settings}>
+          {/* <div>
+            {this.props.product_detail.product_image &&
+              this.props.product_detail.product_image.map((el) => {
+                return <img alt="product" src={el} />;
+              })}
+          </div> */}
+          {/* {product_detail.product_image_big.map((el, idx) => {
+              return (
+                <div key={idx}>
+                  <img alt="product" src={el} />
+                </div>
+              );
+            })} */}
           <div>
-            {this.props.product_detail.map((el) => {
-              return <img alt="product" src={el.product_image}></img>;
-            })}
+            <img
+              alt="mainImg"
+              src="https://www.t2tea.com/dw/image/v2/AASF_PRD/on/demandware.static/-/Sites-masterCatalog_t2/default/dw1815455f/images/products/2020 Aug/T115AE120_apple-maple-muffin_p2.png?sw=555&sh=555&sm=fit"
+            />
           </div>
           <div>
-            <h3>1</h3>
+            <img
+              alt="mainImg"
+              src="https://www.t2tea.com/dw/image/v2/AASF_PRD/on/demandware.static/-/Sites-masterCatalog_t2/default/dw1c74c431/images/products/2020 Aug/T115AE120_apple-maple-muffin_p1.png?sw=555&sh=555&sm=fit"
+            />
           </div>
           <div>
-            <h3>2</h3>
+            <img
+              alt="mainImg"
+              src="https://www.t2tea.com/dw/image/v2/AASF_PRD/on/demandware.static/-/Sites-masterCatalog_t2/default/dw6918da87/images/products/2020 Aug/Apple_Mobile_1024x1024.png?sw=555&sh=555&sm=fit"
+            />
           </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
-          <div>
-            <h3>6</h3>
-          </div>
+          {/* <div>
+              <h3>1</h3>
+            </div>
+            <div>
+              <h3>2</h3>
+            </div>
+            <div>
+              <h3>3</h3>
+            </div>
+            <div>
+              <h3>4</h3>
+            </div>
+            <div>
+              <h3>5</h3>
+            </div>
+            <div>
+              <h3>6</h3>
+            </div> */}
         </Slider>
       </div>
     );
--- src/Pages/ProductDetail/ProductDetail.js
+++ src/Pages/ProductDetail/ProductDetail.js
@@ -1,35 +1,44 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
-import QuantityBox from "./QuantityBox/QuantityBox";
+import QuantityBox from "../../Components/ProductItem/QuantityBox/QuantityBox";
 import "./ProductDetail.scss";
 import MoreBtn from "./MoreBtn/MoreBtn";
+import MainImgSlider from "./MainImgSlider/MainImgSlider";
 
 class ProductDetail extends Component {
   constructor() {
     super();
     this.state = {
       product_detail: [],
-      isFold: false,
+      isFold: "",
+      imgNum: 0,
+      quantity: 1,
     };
   }
   componentDidMount() {
-    fetch("http://localhost:3000/Data/ProductDetail/ProductDetail.json")
+    //fetch("http://localhost:3000/Data/ProductDetail/ProductDetail.json")
+    fetch("http://10.58.7.91:8000/products/463")
       .then((res) => res.json())
       .then((res) => this.setState({ product_detail: res.product_detail }));
+
+    // .then((res) => console.log(res));
   }
 
-  clickMoreBtn = () => {
-    const { isFold } = this.state;
-    this.setState({ isFold: !isFold });
+  clickMoreBtn = (name) => {
+    this.state.isFold === name
+      ? this.setState({ isFold: "" })
+      : this.setState({ isFold: name });
   };
 
-  // handleQuantityChange = (e) => {
-  //   console.log(e.target.value);
-  // };
+  handleQuantityChange = (e) => {
+    this.setState({ quantity: e.target.value });
+  };
 
   render() {
-    const { product_detail, isFold } = this.state;
-    const { clickMoreBtn } = this;
+    console.log(this.state.isFold);
+
+    const { product_detail, isFold, quantity } = this.state;
+    const { clickMoreBtn, clickHandler, handleQuantityChange } = this;
 
     return (
       <div className="ProductDetail">
@@ -37,36 +46,44 @@ class ProductDetail extends Component {
           <ul>
             <li>HOME</li>
             <li>TEA</li>
-            {product_detail.map((el) => {
-              return (
-                <>
-                  <li>{el.product_type}</li>
-                  <li>{el.product_name}</li>
-                </>
-              );
-            })}
+            {/* {product_detail.map((el) => { */}
+            {/* return ( */}
+            {/* <> */}
+            <li>{product_detail.product_type}</li>
+            <li>{product_detail.product_name}</li>
+            {/* </> */}
+            {/* ); */}
+            {/* })} */}
           </ul>
         </header>
         <div className="sectionArea">
           <div className="sectionContainer">
             <section className="left">
               <div className="mainImg">
-                {product_detail.map((el) => {
-                  return <img alt="product" src={el.product_image}></img>;
-                })}
+                {/* {product_detail.map((el) => {
+                    return <img alt="product" src={el.product_image}></img>;
+                })} */}
+                <MainImgSlider product_detail={product_detail} />
               </div>
               <div className="subImg">
+                {/* {product_detail.product_image &&
+                  product_detail.product_image.map((el, idx) => {
+                    return <img key={idx} alt="subImg" src={el} />;
+                  })} */}
                 <img
+                  onClick={clickHandler}
                   alt="product 1"
-                  src="https://www.t2tea.com/dw/image/v2/AASF_PRD/on/demandware.static/-/Sites-masterCatalog_t2/default/dw344ddde6/images/products/2020 Aug/T135AE040_jam-on-toast_p2.png?sw=100&sh=100&sm=fit"
+                  src="https://www.t2tea.com/dw/image/v2/AASF_PRD/on/demandware.static/-/Sites-masterCatalog_t2/default/dw1815455f/images/products/2020 Aug/T115AE120_apple-maple-muffin_p2.png?sw=100&sh=100&sm=fit"
                 />
                 <img
+                  onClick={clickHandler}
                   alt="product 2"
-                  src="https://www.t2tea.com/dw/image/v2/AASF_PRD/on/demandware.static/-/Sites-masterCatalog_t2/default/dwd5cc57fc/images/products/2020 Aug/T135AE040_jam-on-toast_p1.png?sw=100&sh=100&sm=fit"
+                  src="https://www.t2tea.com/dw/image/v2/AASF_PRD/on/demandware.static/-/Sites-masterCatalog_t2/default/dw1c74c431/images/products/2020 Aug/T115AE120_apple-maple-muffin_p1.png?sw=100&sh=100&sm=fit"
                 />
                 <img
+                  onClick={clickHandler}
                   alt="product 3"
-                  src="https://www.t2tea.com/dw/image/v2/AASF_PRD/on/demandware.static/-/Sites-masterCatalog_t2/default/dwfcfb83f8/images/products/2020 Aug/Jam_Mobile_1024x1024.png?sw=100&sh=100&sm=fit"
+                  src="https://www.t2tea.com/dw/image/v2/AASF_PRD/on/demandware.static/-/Sites-masterCatalog_t2/default/dw6918da87/images/products/2020 Aug/Apple_Mobile_1024x1024.png?sw=100&sh=100&sm=fit"
                 />
               </div>
               <div className="sns">
@@ -79,14 +96,19 @@ class ProductDetail extends Component {
             </section>
             <section className="right">
               <div className="mainInfo">
-                {product_detail.map((el) => {
-                  return (
-                    <>
-                      <div className="productName">{el.product_name}</div>
-                      <div className="price">A${el.product_price}</div>
-                    </>
-                  );
-                })}
+                {/* {product_detail.map((el) => { */}
+                {/* return ( */}
+                {/* <> */}
+                <div className="productName">{product_detail.product_name}</div>
+                <div className="price">
+                  A$
+                  {product_detail.product_price === null
+                    ? product_detail.size_price[0]
+                    : product_detail.product_price}
+                </div>
+                {/* </> */}
+                {/* ); */}
+                {/* })} */}
                 <div className="review">
                   <span className="rating">
                     <span>★★★★★</span>
@@ -98,10 +120,10 @@ class ProductDetail extends Component {
               </div>
               <div className="subInfo">
                 <QuantityBox
-                // id="quantity"
-                // name={1}
-                // list={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
-                // handleClick={this.handleQuantityChange}
+                  id="quantity"
+                  name={quantity}
+                  list={[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
+                  handleClick={handleQuantityChange}
                 />
                 <div className="productBtn">
                   <button className="addToBag">Add to bag</button>
@@ -115,73 +137,97 @@ class ProductDetail extends Component {
                 </div>
               </div>
               <div className="tabInfo">
-                {product_detail.map((el) => {
-                  return (
-                    <>
-                      <div className="tab">
-                        <span>Description</span>
-                        <MoreBtn clickMoreBtn={clickMoreBtn} />
-                      </div>
-                      <div className="subTab">
-                        <div className={!isFold ? "hideTab" : "showTab"}>
-                          <div>{el.description}</div>
-                        </div>
-                      </div>
-                      <div className="tab">
-                        <span>Ingredients</span>
-                        <MoreBtn clickMoreBtn={clickMoreBtn} />
-                      </div>
-                      <div className="subTab">
-                        <div className={isFold ? "hideTab" : "showTab"}>
-                          <div>{el.ingredients}</div>
-                        </div>
-                        <div></div>
-                      </div>
-                      <div className="tab">
-                        <span>Brewing guide</span>
-                        <MoreBtn clickMoreBtn={clickMoreBtn} />
-                      </div>
-                      <div className="subTab">
-                        <div className={isFold ? "hideTab" : "showTab"}>
-                          <div>
-                            <img
-                              alt="quantity"
-                              src="https://www.t2tea.com/on/demandware.static/Sites-UNI-T2-APAC-Site/-/default/dwe72e4d99/images/brewing-quantity.png"
-                            />
-                            <span>{el.brewing_quantity}</span>
-                            <img
-                              alt="time"
-                              src="https://www.t2tea.com/on/demandware.static/Sites-UNI-T2-APAC-Site/-/default/dw0539ea3b/images/brewing-time.png"
-                            />
-                            <span>{el.brewing_time}</span>
-                            <img
-                              alt="temp"
-                              src="https://www.t2tea.com/on/demandware.static/Sites-UNI-T2-APAC-Site/-/default/dw18ddadb8/images/brewing-degrees.png"
-                            />
-                            <span>{el.brewing_temp}</span>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="tab">
-                        <span>Shippings & returns</span>
-                        <MoreBtn clickMoreBtn={clickMoreBtn} />
-                      </div>
-                      <div className="subTab">
-                        <div className={isFold ? "hideTab" : "showTab"}>
-                          We ship anywhere in the world! Check out our delivery
-                          and returns policy here.
-                        </div>
-                      </div>
-                      <div className="tab">
-                        <span>Customer Reviews</span>
-                        <MoreBtn clickMoreBtn={clickMoreBtn} />
-                      </div>
-                      <div className="subTab">
-                        <div></div>
-                      </div>
-                    </>
-                  );
-                })}
+                {/* {product_detail.map((el) => { */}
+                {/* return ( */}
+                {/* <> */}
+                <div className="tab">
+                  <span>Description</span>
+                  <MoreBtn
+                    // className={isFold === "Description" ? "rotateBtn" : ""}
+                    clickMoreBtn={() => clickMoreBtn("Description")}
+                  />
+                </div>
+                <div className="subTab">
+                  <div
+                    className={isFold === "Description" ? "showTab" : "hideTab"}
+                  >
+                    <div>{product_detail.description}</div>
+                  </div>
+                </div>
+                <div className="tab">
+                  <span>Ingredients</span>
+                  <MoreBtn clickMoreBtn={() => clickMoreBtn("Ingredients")} />
+                </div>
+                <div className="subTab">
+                  <div
+                    className={isFold === "Ingredients" ? "showTab" : "hideTab"}
+                  >
+                    <div>{product_detail.ingredients}</div>
+                  </div>
+                </div>
+                <div className="tab">
+                  <span>Brewing guide</span>
+                  <MoreBtn clickMoreBtn={() => clickMoreBtn("Brewing guide")} />
+                </div>
+                <div className="subTab">
+                  <div
+                    className={
+                      isFold === "Brewing guide" ? "showTab" : "hideTab"
+                    }
+                  >
+                    <div>
+                      <img
+                        alt="quantity"
+                        src="https://www.t2tea.com/on/demandware.static/Sites-UNI-T2-APAC-Site/-/default/dwe72e4d99/images/brewing-quantity.png"
+                      />
+                      <span>{product_detail.brewing_quantity}</span>
+                      <img
+                        alt="time"
+                        src="https://www.t2tea.com/on/demandware.static/Sites-UNI-T2-APAC-Site/-/default/dw0539ea3b/images/brewing-time.png"
+                      />
+                      <span>{product_detail.brewing_time}</span>
+                      <img
+                        alt="temp"
+                        src="https://www.t2tea.com/on/demandware.static/Sites-UNI-T2-APAC-Site/-/default/dw18ddadb8/images/brewing-degrees.png"
+                      />
+                      <span>{product_detail.brewing_temp}</span>
+                    </div>
+                  </div>
+                </div>
+                <div className="tab">
+                  <span>Shippings & returns</span>
+                  <MoreBtn
+                    clickMoreBtn={() => clickMoreBtn("Shippings & returns")}
+                  />
+                </div>
+                <div className="subTab">
+                  <div
+                    className={
+                      isFold === "Shippings & returns" ? "showTab" : "hideTab"
+                    }
+                  >
+                    <div>
+                      We ship anywhere in the world! Check out our delivery and
+                      returns policy here.
+                    </div>
+                  </div>
+                </div>
+                <div className="tab">
+                  <span>Customer Reviews</span>
+                  <MoreBtn
+                    clickMoreBtn={() => clickMoreBtn("Customer Reviews")}
+                  />
+                </div>
+                <div
+                  className={
+                    isFold === "Customer Reviews" ? "showTab" : "hideTab"
+                  }
+                >
+                  <div></div>
+                </div>
+                {/* </> */}
+                {/* ); */}
+                {/* })} */}
               </div>
             </section>
           </div>
