81b8baa0130871f539c4893cae025aec337e90ba
masangwon <ssamwonn@gmail.com>
2020-08-28 12:19:44 +0900

    [Add]라우터 수정사항 -주소 수정

--- src/Routes.js
+++ src/Routes.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Main from "./Pages/Main/Main";
 import Login from "./Pages/Login/Login";
+import Join from "./Pages/Join/Join";
 import ProductList from "./Pages/ProductList/ProductList";
 import ProductDetail from "./Pages/ProductDetail/ProductDetail";
 
@@ -12,6 +13,7 @@ class Routes extends Component {
         <Switch>
           <Route exact path="/" component={Main} />
           <Route exact path="/account" component={Login} />
+          <Route exact path="/account/join" component={Join} />
           <Route exact path="/shop" component={ProductList} />
           <Route exact path="/detail" component={ProductDetail} />
         </Switch>
